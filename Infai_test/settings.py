f6c53d9c6b0e560d4c01a55ccc55ce1ea4984f42
Preetzl <peetz@infai.org>
2023-11-08 11:02:40 +0100

    Saving sensitive data in .env file and update the settings.py

--- Infai_test/settings.py
+++ Infai_test/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+
+env= environ.Env()
+environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2m2u=z(htevm5zg4wl-&n5%^o=#0%c9_*j(i7*3xjqb*wcrv@+'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,10 +86,10 @@ WSGI_APPLICATION = 'Infai_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
         'HOST': 'db',
         'PORT': 5432 ,
                 }
